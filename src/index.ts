71bbb1f1cd7311b0de9ee5947ebc179977ac11bc
Work <test@tests-MacBook-Air.local>
2025-05-20 03:50:17 +0100

    Bismillah

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import express, { Request, Response } from 'express';
+const app = express();
+app.get('/', (req: Request, res: Response) => { res.send('Hello'); });
+app.listen(3000, () => { console.log('Running on port 3000'); });
